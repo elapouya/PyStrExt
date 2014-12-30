f79c6411f0c6cc98c40b5fad26f57e2ecbbf4c13
Eric Lapouyade <elapouya@gmail.com>
2014-12-30 01:52:20 +0100

    add url in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pystrext',
-      version='0.1.1',
+      version='0.1.2',
       description='Python string extension',
       classifiers=[
           "Intended Audience :: Developers",
@@ -10,7 +10,7 @@ setup(name='pystrext',
           "Programming Language :: Python :: 2.7",
       ],
       keywords='string helpers',
-      url='',
+      url='https://pypi.python.org/pypi/pystrext',
       author='Eric Lapouyade',
       author_email='elapouya@gmail.com',
       license='LGPL 2.1',
